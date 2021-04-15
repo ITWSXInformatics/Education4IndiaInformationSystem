024ab039eeca18257e78f9aae623165fcf7dee86
Roma <paranr@rpi.edu>
2021-04-15 15:18:00 -0400

    Took out about and help tabs, added a search bar for the table view

--- script.js
+++ script.js
@@ -142,6 +142,35 @@ $('th').on('click', function(){
 })
 
 
+
+
+
+
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("dattable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+
+
+
 buildTable(datArray)
 
 function buildTable(data){
